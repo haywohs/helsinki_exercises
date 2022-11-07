4df23460b0ea9c5e95f6152ebfb62f8c00688dcb
yuyun <ru8391@gmail.com>
2022-11-07 16:42:09 -0700

    P1 .12 ~ .14

--- P1/p1/src/App.js
+++ P1/p1/src/App.js
@@ -1,74 +1,77 @@
 import { useState } from 'react';
 
-
-const StatisticLine = ({ text, value }) => {
+const Anecdotes = ({ text, voteCount }) => {
   return (
-    <>
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
-    </>
+    <div>
+      <h4>{text}</h4>
+      <p>has {voteCount} votes</p>
+    </div>
   )
 }
 
-const Statistics = (props) => {
-  const good = props.goodNum;
-  const bad = props.badNum;
-  const neutral = props.neutralNum;
+const Button = ({ click, text }) => (
+  <button onClick={click} >{text}</button>
+)
 
-  const all = good + bad + neutral;
-  const average = (good - bad) / all;
-  const positive = (good / all) * 100 + "%";
+const HighestText = ({ texts, voteCount }) => {
+  const high = Math.max(...voteCount);
+  //console.log(voteCount); //print an array
+  //console.log(...voteCount); //print strings
+  const index = voteCount.indexOf(high);
+  console.log(high)
 
-  if (all <= 0) {
+  if (high === 0) {
     return (
-      <p>No feedback given</p>
+      <div>
+        <p>Not vote yet</p>
+      </div>
     )
   }
 
   return (
     <div>
-      <table>
-        <tbody>
-          <StatisticLine text="Good" value={good} />
-          <StatisticLine text="Neutral" value={neutral} />
-          <StatisticLine text="Bad" value={bad} />
-          <StatisticLine text="All" value={all} />
-          <StatisticLine text="Average" value={average} />
-          <StatisticLine text="Positive" value={positive} />
-        </tbody>
-      </table>
+      <h4>{texts[index]}</h4>
+      <p>has {high} votes</p>
     </div>
   )
 }
 
-
-
-const Button = ({ click, text }) => (
-  <button onClick={click}>{text}</button>
-)
-
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
 
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const [allVotes, setAll] = useState(Array(7).fill(0));
 
-  const handleG = () => setGood(good + 1);
-  const handleB = () => setBad(bad + 1);
-  const handleN = () => setNeutral(neutral + 1);
+  const voteClick = () => {
+    const voteArray = [...allVotes];
+    voteArray[selected] += 1
+    setAll(voteArray);
+  }
+
+  const aneClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+
 
 
   return (
     <>
-      <h1>give feedback</h1>
-      <Button click={handleG} text="Good" />
-      <Button click={handleN} text="Neutral" />
-      <Button click={handleB} text="Bad" />
+      <h1>Anecdote of the day</h1>
+      <Anecdotes text={anecdotes[selected]} voteCount={allVotes[selected]} />
+      <Button click={aneClick} text="anecdotes" />
+      <Button click={voteClick} text="vote" />
 
-      <h1>Statistics</h1>
-      <Statistics goodNum={good} badNum={bad} neutralNum={neutral} />
+      <h1>Anecdote with most votes</h1>
+      <HighestText texts={anecdotes} voteCount={allVotes} />
     </>
   )
 }
